47b4733bade12f3a700859fc0a245a4b8697df1f
Simon Cheng <simon.cheng@shopify.com>
2017-07-20 23:01:47 -0400

    Poloniex API Working + bootstrap templating

--- poloniex_api.js
+++ poloniex_api.js
@@ -1,5 +1,16 @@
 
 $(document).ready(function() {
 
+fetchPoloniex()
 });
+
+
+function fetchPoloniex () {
+   $.get(
+    "https://poloniex.com/public?command=returnTicker",
+    function(data) {
+      console.log(data)
+    }
+  );
+}
 
