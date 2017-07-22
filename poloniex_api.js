965cadc83d57c8ccb95570848c4a895e76e04b89
Simon Cheng <simon.cheng@shopify.com>
2017-07-21 22:59:59 -0400

    API Working

--- poloniex_api.js
+++ poloniex_api.js
@@ -5,12 +5,45 @@ $(document).ready(function() {
   });
 });
 
-
 function reloadPoloniex() {
    $.get(
     "https://poloniex.com/public?command=returnTicker",
-    function(data) {
+    function(raw_data) {
+
+      data = []
+
+      for (var crypto in raw_data) {
+        temp = {};
+        temp = raw_data[crypto]
+        temp["currency"] = crypto
+        data.push(temp)
+      }
+
       console.log(data)
+
+      var body = $('#poloniexTable'),
+
+      props = [
+        "currency",
+        "baseVolume",
+        "high24hr",
+        "highestBid",
+        "id",
+        "isFrozen",
+        "last",
+        "low24hr",
+        "lowestAsk",
+        "percentChange",
+        "quoteVolume"
+      ];
+
+      $.each(data, function(i, data) {
+        var tr = $('<tr>');
+        $.each(props, function(i, prop) {
+          $('<td>').html(data[prop]).appendTo(tr);
+        });
+        body.append(tr);
+      });
     }
   );
 }
